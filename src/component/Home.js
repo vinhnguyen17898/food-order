1af5822cbfeda4aea831220c2212b9e3c437897a
Vinh Nguyen <toiyeudanang1998@gmail.com>
2023-10-08 16:41:28 +0700

    a

--- src/component/Home.js
+++ src/component/Home.js
@@ -4,6 +4,7 @@ import './Home.css'
 import React, { useRef, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
+import axios from "axios";
 
 const Home = () => {
     const [step, setStep] = useState(1);
@@ -17,20 +18,38 @@ const Home = () => {
 
     const shoppeStep3 = () => {
         setStep(3);
-        fetch("https://gappapi.deliverynow.vn/api/delivery/get_from_url?url=ho-chi-minh/com-ba-ghien-nguyen-van-troi", {
-                method: 'GET',
-                mode: 'no-cors',
-                headers: {
-                    'X-Custom-Header': 'header value'
-                }
+        let config = {
+            method: 'get',
+            url: 'https://gappapi.deliverynow.vn/api/delivery/get_from_url?url=ho-chi-minh/com-ba-ghien-nguyen-van-troi',
+            headers: {
+                "Cache-Control": "no-cache",
+                "Content-Type": "application/x-www-form-urlencoded",
+                'X-FoodyAccessToken': '',
+                'X-Foody-Api-Version': '1',
+                'X-Foody-App-Type': '1004',
+                'X-Foody-Client-Id': '',
+                'X-Foody-Client-Language': 'vi',
+                'X-Foody-Client-Type': '1',
+                'X-Foody-Client-Version': '3.0.0',
+                'X-Sap-Ri': 'ef6c216559a26b6011140d38562609a2f46c5111f0762022',
+                'X-Custom-Header': 'header value',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Headers': '*',
+                'Access-Control-Allow-Credentials': true,
+                "Access-Control-Allow-Origin": "*",
+       "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"
+            },
+            credentials: 'same-origin',
+        };
+
+        axios.request(config)
+            .then((response) => {
+                console.log(JSON.stringify(response.data));
             })
-            .then(response => {
-                console.log(response);
-                return response.json()
-            })
-            .then(data => {
-                console.log(data);
-            })
+            .catch((error) => {
+                console.log(error);
+            });
+
     }
 
     return (<>
